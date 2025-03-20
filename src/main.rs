c3b07b9cef0441dd4056869c1ac63605afa2e083
Julien Gautier <gautier.julien@gmail.com>
2025-03-20 22:13:19 +0100

    works with multiple services

--- src/main.rs
+++ src/main.rs
@@ -28,13 +28,6 @@ struct Service {
     last_result: bool,
 }
 
-// struct ServiceResult {
-//     function_name: String,
-//     if_true: String,
-//     if_false: String,
-//     bird_return_value: bool,
-// }
-
 struct Config {
     generated_file_path: OsString,
 }
@@ -45,14 +38,22 @@ async fn main() {
         generated_file_path: "birdwatcher_generated.conf".into(),
     };
 
-    let service_defintions = [ServiceDefinition {
-        function_name: "match_true".to_string(),
-        command: "/bin/true".to_string(),
-        prefixes: None,
-        interval: Duration::from_secs(1),
-    }];
+    let service_defintions = [
+        ServiceDefinition {
+            function_name: "match_true".to_string(),
+            command: "/bin/true".to_string(),
+            prefixes: None,
+            interval: Duration::from_secs(1),
+        },
+        ServiceDefinition {
+            function_name: "match_false".to_string(),
+            command: "/bin/false".to_string(),
+            prefixes: None,
+            interval: Duration::from_secs(2),
+        },
+    ];
 
-    let mut services: Vec<Service> = service_defintions
+    let services: Vec<Service> = service_defintions
         .iter()
         .map(|def| Service {
             def: def.clone(),
@@ -62,67 +63,56 @@ async fn main() {
         })
         .collect();
 
-    let mut services: Arc<Mutex<Vec<Service>>> = Arc::new(Mutex::new(services));
-
-    // let mut functions: HashMap<String, bool> = HashMap::new();
-    // services.iter().for_each(|service| {
-    //     functions.insert(service.function_name.clone(), false);
-    // });
-
-    // let (tx, mut rx) = tokio::sync::mpsc::channel(1);
+    let services: Arc<Mutex<Vec<Service>>> = Arc::new(Mutex::new(services));
 
     let mut join_set = JoinSet::new();
 
-    let join_handles =
-        service_defintions
-            .into_iter()
-            .enumerate()
-            .for_each(|(service_nb, service_def)| {
-                // let service_def = service.def.clone();
-                // let service_def = service_def;
-                // let service_nb = service_nb;
-                let services = services.clone();
-                let generated_file_path = config.generated_file_path.clone();
-                join_set.spawn(async move {
-                    loop {
-                        std::print!(
-                            "Regen function {}, Launching command {}",
-                            service_def.function_name,
-                            service_def.command
-                        );
-                        let result = tokio::process::Command::new(service_def.command.clone())
-                            .output()
-                            .await;
-                        let return_value = match result {
-                            Ok(o) => {
-                                if o.status.success() {
-                                    true
-                                } else {
-                                    false
-                                }
-                            }
-                            Err(e) => {
-                                eprint!(
-                                    "Could not launch command \'{}\'. e = {}",
-                                    service_def.command, e
-                                );
+    service_defintions
+        .into_iter()
+        .enumerate()
+        .for_each(|(service_nb, service_def)| {
+            println!("Staring {}", service_def.function_name);
+            let services = services.clone();
+            let generated_file_path = config.generated_file_path.clone();
+            join_set.spawn(async move {
+                loop {
+                    println!(
+                        "Regen function {}, Launching command {}",
+                        service_def.function_name, service_def.command
+                    );
+                    let result = tokio::process::Command::new(service_def.command.clone())
+                        .output()
+                        .await;
+                    let return_value = match result {
+                        Ok(o) => {
+                            if o.status.success() {
+                                true
+                            } else {
                                 false
                             }
-                        };
-                        {
-                            let mut services_lock = services.lock().unwrap();
-                            services_lock[service_nb].last_result = return_value;
-                            write_bird_function(&generated_file_path, services_lock.as_slice());
                         }
-
-                        tokio::time::sleep(service_def.interval).await;
+                        Err(e) => {
+                            eprintln!(
+                                "Could not launch command \'{}\'. e = {}",
+                                service_def.command, e
+                            );
+                            false
+                        }
+                    };
+                    {
+                        let mut services_lock = services.lock().unwrap();
+                        services_lock[service_nb].last_result = return_value;
+                        write_bird_function(&generated_file_path, services_lock.as_slice());
                     }
-                });
-            });
 
-    print!("All services launched");
+                    tokio::time::sleep(service_def.interval).await;
+                }
+            });
+        });
+
+    println!("All services launched");
     if let Some(t) = join_set.join_next().await {
-        eprint!("Task failed with {}", t.err().unwrap())
+        eprintln!("Task failed with {}", t.err().unwrap())
     }
 }
 
@@ -137,7 +127,14 @@ fn write_bird_function(generated_file_path: &OsStr, services: &[Service]) {
             } else {
                 &service.if_false
             };
-            format!("function {function_name} {{ return {return_value}; }}",)
+            format!(
+                "
+function {function_name} -> bool
+{{
+    return {return_value};
+}}
+            ",
+            )
         })
         .join("\n");
 
