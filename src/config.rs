91e539eee026676a7e79c3f43c94b37ddd9cab2a
Julien Gautier <gautier.julien@gmail.com>
2025-03-25 23:48:21 +0100

    Deserialize from toml file

---
+++ src/config.rs
@@ -0,0 +1,147 @@
+mod raw {
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Clone, Deserialize, Serialize)]
+    pub struct Config {
+        pub generated_file_path: String,
+        pub reload_command: String,
+        pub reload_command_args: Vec<String>,
+        pub reload_timeout: f32,
+        pub service_definitions: Vec<ServiceDefinition>,
+    }
+
+    #[derive(Clone, Deserialize, Serialize)]
+    pub struct ServiceDefinition {
+        pub function_name: String,
+        pub command: String,
+        pub args: Vec<String>,
+        pub interval: f32,
+        pub command_timeout: f32,
+        /// Number of consecutive failure to consider the service unhealthy
+        pub fall: u32,
+        /// Number of consecutive failure to consider the service healthy
+        pub rise: u32,
+    }
+}
+
+use itertools::Itertools;
+use serde::{Deserialize, Serialize};
+use std::time::Duration;
+
+#[derive(Clone, Deserialize, Serialize)]
+pub struct Config {
+    pub generated_file_path: String,
+    pub reload_command: String,
+    pub reload_command_args: Vec<String>,
+    pub reload_timeout: Duration,
+    pub service_definitions: Vec<ServiceDefinition>,
+}
+
+impl Config {
+    pub fn load_from_file(filepath: &str) -> Config {
+        let raw_config: Result<crate::config::raw::Config, _> =
+            toml::from_str(&std::fs::read_to_string(filepath).unwrap());
+        match raw_config {
+            Err(e) => {
+                println!("{}", e);
+                panic!();
+            }
+
+            Ok(raw_config) => Config {
+                generated_file_path: raw_config.generated_file_path,
+                reload_command: raw_config.reload_command,
+                reload_command_args: raw_config.reload_command_args,
+                reload_timeout: Duration::from_secs_f32(raw_config.reload_timeout),
+                service_definitions: raw_config
+                    .service_definitions
+                    .into_iter()
+                    .map(|raw| ServiceDefinition {
+                        function_name: raw.function_name,
+                        command: raw.command,
+                        args: raw.args,
+                        interval: Duration::from_secs_f32(raw.interval),
+                        command_timeout: Duration::from_secs_f32(raw.command_timeout),
+                        fall: raw.fall,
+                        rise: raw.rise,
+                    })
+                    .collect_vec(),
+            },
+        }
+    }
+}
+
+#[derive(Clone, Deserialize, Serialize)]
+pub struct ServiceDefinition {
+    pub function_name: String,
+    pub command: String,
+    pub args: Vec<String>,
+    pub interval: Duration,
+    pub command_timeout: Duration,
+    /// Number of consecutive failure to consider the service unhealthy
+    pub fall: u32,
+    /// Number of consecutive failure to consider the service healthy
+    pub rise: u32,
+}
+
+#[derive(Debug)]
+pub enum ServiceState {
+    /// In `Failure` state, count the number of success.
+    /// When the number goes above `rise`, switch to `Success` state
+    /// Any failure reset the counter to 0
+    Failure { nb_of_success: u32 },
+    /// In `Success` state, count the number of failure.
+    /// When the number goes above `fall`, switch to `Failure` state
+    /// Any failure reset the counter to 0
+    Success { nb_of_failure: u32 },
+}
+
+impl ServiceState {
+    pub fn update_with(
+        &self,
+        return_value: bool,
+        service_def: &ServiceDefinition,
+    ) -> (ServiceState, bool) {
+        match self {
+            ServiceState::Failure { nb_of_success } => {
+                if return_value {
+                    if nb_of_success + 1 >= service_def.rise {
+                        // Switch to rise
+                        (ServiceState::Success { nb_of_failure: 0 }, true)
+                    } else {
+                        // Another success, but not enough to rise
+                        (
+                            ServiceState::Failure {
+                                nb_of_success: nb_of_success + 1,
+                            },
+                            false,
+                        )
+                    }
+                } else
+                /* A failure on failure */
+                {
+                    (ServiceState::Failure { nb_of_success: 0 }, false)
+                }
+            }
+            ServiceState::Success { nb_of_failure } => {
+                if return_value {
+                    (ServiceState::Success { nb_of_failure: 0 }, false)
+                } else
+                /* A new failure. Should we switch to Failure? */
+                {
+                    // Yes, switch to Failure
+                    if nb_of_failure + 1 >= service_def.fall {
+                        (ServiceState::Failure { nb_of_success: 0 }, true)
+                    } else {
+                        // No. Another failure, but not enough to fall
+                        (
+                            ServiceState::Success {
+                                nb_of_failure: nb_of_failure + 1,
+                            },
+                            false,
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,10 @@
-use std::{ffi::OsString, fs::File, io::Write, process::ExitCode, time::Duration};
+mod config;
+
+use std::{fs::File, io::Write};
 
 use tokio::{process::Command, task::JoinSet, time::timeout};
 
-#[derive(Clone)]
-struct ServiceDefinition {
-    function_name: String,
-    command: String,
-    args: Vec<String>,
-    interval: Duration,
-    command_timeout: Duration,
-    /// Number of consecutive failure to consider the service unhealthy
-    fall: u32,
-    /// Number of consecutive failure to consider the service healthy
-    rise: u32,
-}
-
-#[derive(Debug)]
-enum ServiceState {
-    /// In `Failure` state, count the number of success.
-    /// When the number goes above `rise`, switch to `Success` state
-    /// Any failure reset the counter to 0
-    Failure { nb_of_success: u32 },
-    /// In `Success` state, count the number of failure.
-    /// When the number goes above `fall`, switch to `Failure` state
-    /// Any failure reset the counter to 0
-    Success { nb_of_failure: u32 },
-}
-
-impl ServiceState {
-    fn update_with(
-        &self,
-        return_value: bool,
-        service_def: &ServiceDefinition,
-    ) -> (ServiceState, bool) {
-        match self {
-            ServiceState::Failure { nb_of_success } => {
-                if return_value {
-                    if nb_of_success + 1 >= service_def.rise {
-                        // Switch to rise
-                        (ServiceState::Success { nb_of_failure: 0 }, true)
-                    } else {
-                        // Another success, but not enough to rise
-                        (
-                            ServiceState::Failure {
-                                nb_of_success: nb_of_success + 1,
-                            },
-                            false,
-                        )
-                    }
-                } else
-                /* A failure on failure */
-                {
-                    (ServiceState::Failure { nb_of_success: 0 }, false)
-                }
-            }
-            ServiceState::Success { nb_of_failure } => {
-                if return_value {
-                    (ServiceState::Success { nb_of_failure: 0 }, false)
-                } else
-                /* A new failure. Should we switch to Failure? */
-                {
-                    // Yes, switch to Failure
-                    if nb_of_failure + 1 >= service_def.fall {
-                        (ServiceState::Failure { nb_of_success: 0 }, true)
-                    } else {
-                        // No. Another failure, but not enough to fall
-                        (
-                            ServiceState::Success {
-                                nb_of_failure: nb_of_failure + 1,
-                            },
-                            false,
-                        )
-                    }
-                }
-            }
-        }
-    }
-}
-
-struct Service {
-    def: ServiceDefinition,
-    state: ServiceState,
-}
-
-#[derive(Clone)]
-struct Config {
-    generated_file_path: OsString,
-    reload_command: String,
-    reload_command_args: Vec<String>,
-    reload_timeout: Duration,
-    service_definitions: Vec<ServiceDefinition>,
-}
+use config::{Config, ServiceState};
 
 struct ServiceCommandResult {
     service_id: usize,
@@ -99,32 +13,7 @@ struct ServiceCommandResult {
 
 #[tokio::main]
 async fn main() {
-    let config = Config {
-        generated_file_path: "birdwatcher_generated.conf".into(),
-        reload_command: "birdc".to_string(),
-        reload_command_args: vec!["configure".to_string()],
-        reload_timeout: Duration::from_secs(2),
-        service_definitions: vec![
-            ServiceDefinition {
-                function_name: "match_true".to_string(),
-                command: "/bin/ls".to_string(),
-                args: vec!["1".to_string()],
-                interval: Duration::from_secs(1),
-                command_timeout: Duration::from_secs(1),
-                fall: 1,
-                rise: 3,
-            },
-            ServiceDefinition {
-                function_name: "match_false".to_string(),
-                command: "/bin/ls".to_string(),
-                args: vec!["2".to_string()],
-                interval: Duration::from_secs(2),
-                command_timeout: Duration::from_secs(1),
-                fall: 2,
-                rise: 2,
-            },
-        ],
-    };
+    let config: Config = Config::load_from_file("birdwatcher.conf");
 
     // Contains the only mutable state: a counter for each service
     let mut service_states: Vec<ServiceState> = config
