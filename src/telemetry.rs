c97cacb8a52d7f71f411761c005f1764f1fd258f
Julien Gautier <gautier.julien@gmail.com>
2025-11-04 22:19:43 +0100

    enable log/trace sending

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -83,18 +83,18 @@ fn build_tracing_subscriber(
 
     // To prevent a telemetry-induced-telemetry loop
     // See: https://github.com/open-telemetry/opentelemetry-rust/pull/3084/files#diff-b3b130c078a1640592a5defce7c923f8343047e7f18c71e0707bc4a0f094e731L69
-    /* let filter_otel = EnvFilter::new("info")
+    let filter_otel = EnvFilter::new("info")
         .add_directive("hyper=off".parse().unwrap())
         .add_directive("tonic=off".parse().unwrap())
         .add_directive("h2=off".parse().unwrap())
         .add_directive("reqwest=off".parse().unwrap());
-    let log_otlp_exporter_layer = otel_layer.with_filter(filter_otel); */
+    let log_otlp_exporter_layer = otel_layer.with_filter(filter_otel);
 
     // Use the tracing subscriber `Registry`, or any other subscriber
     // that impls `LookupSpan`
     let subscriber = Registry::default()
-        // .with(trace_otlp_exporter_layer)
-        // .with(log_otlp_exporter_layer)
+        .with(trace_otlp_exporter_layer)
+        .with(log_otlp_exporter_layer)
         .with(log_stdout_exporter_layer);
 
     Ok(subscriber)
